597eebfcd2f76261562b41b5c2c9007973bb4964
Danilo Giacobo <danilogiacobo@gmail.com>
2021-04-19 14:09:42 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/danilogiacobo/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.danilogiacobo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danilogiacobo.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
